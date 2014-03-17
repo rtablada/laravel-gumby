bf0c942f82ca3dc512a1a5df9940c48387ff4c5b
Ryan Tablada <ryan@Ryans-MacBook-Pro.local>
2014-03-17 04:13:13 -0500

    Fixing some issues

--- clone-assets.sh
+++ clone-assets.sh
@@ -1,15 +1,17 @@
 echo ">>> Cloning Gumby assets";
-mkdir public/js;
+mkdir -p public/js;
+mkdir -p public/css;
+mkdir -p public/fonts;
 cp lib/bower/gumby/js/libs/modernizr-2.6.2.min.js public/js/modernizr.js;
 cp lib/bower/jquery/dist/jquery.min.js public/js/jquery.min.js;
 cp lib/bower/jquery/dist/jquery.min.map public/js/jquery.min.map;
+cp -r bower_componenets/gumby/fonts/icons public/fonts/;
 
 mkdir -p assets/sass/var;
 cp lib/bower/gumby/sass/gumby.scss assets/sass/;
-cp lib/bower/gumby/sass/_settings.scss assets/sass/_settings.scss;
+cp lib/bower/gumby/sass/_settings.scss assets/sass/var/_settings.scss;
 cp lib/bower/gumby/sass/_fonts.scss assets/sass/_font.scss;
 cp lib/bower/gumby/sass/_custom.scss assets/sass/_custom.scss;
-cp lib/bower/gumby/config.rb .;
-cp -r bower_componenets/gumby/fonts/icons public/fonts/;
+cp lib/bower/gumby/config.rb config.rb;
 compass compile;
 echo '{ "build": { "gumbyPath": "lib/bower/gumby", "outPath": "pubclic/js" } }' > gumby.json;
