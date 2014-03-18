cf8fbe6d0644d81666ed730e4cde71fb59412d5e
Ryan Tablada <ryan@Ryans-MacBook-Pro.local>
2014-03-17 22:10:41 -0500

    I said... NO BACKUPS

--- compass-setup.sh
+++ compass-setup.sh
@@ -4,3 +4,4 @@ sed -i '.bak' 's/"css"/"public\/css"/g' config.rb;
 sed -i '.bak' 's/"sass"/"assets\/sass"/g' config.rb;
 sed -i '.bak' 's/"img"/"public\/img"/g' config.rb;
 sed -i '.bak' 's/"sass\/extensions"/"lib\/bower\/gumby\/sass\/extensions"/g' config.rb;
+rm config.rb.bak;
